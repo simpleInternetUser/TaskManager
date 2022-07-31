2b87d3ef5371d499b340bfacfa65ddbc176f2aa1
Taras <simple_internet_user@zohomail.com>
2022-07-31 03:49:17 +0300

    Написал функцию-обработчик для корневого пути /

---
+++ handlers/handlers.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/simpleInternetUser/TaskManager/tasks"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, tasks.ShowTasks())
+}
