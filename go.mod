332cdce85cfe2823b71faa65f269ffa7386cb0de
Taras <simple_internet_user@zohomail.com>
2022-07-31 05:00:01 +0300

    Путаница с пакетами

--- go.mod
+++
@@ -1,5 +0,0 @@
-module handlers
-
-go 1.18
-
-require github.com/simpleInternetUser/TaskManager v0.0.0-20220731005658-34202743edf5
--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 
-	"github.com/simpleInternetUser/TaskManager/handlers"
+	"github.com/simpleInternetUser/TaskManager/tasks"
 )
 
+func Index(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, tasks.GetAllTasks())
+}
+
 func main() {
-	http.HandleFunc("/", handlers.Index)
+	http.HandleFunc("/", Index)
 	http.ListenAndServe(":8080", nil)
 }
