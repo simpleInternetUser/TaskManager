0649ce8be21d9da36841fd71b594fc0ad5848e6b
Taras <simple_internet_user@zohomail.com>
2022-07-31 03:24:54 +0300

    Добавлена структура таски а также функция получения даных

---
+++ tasks/tasks.go
@@ -0,0 +1,34 @@
+package tasks
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+)
+
+type Task struct {
+	Id          int
+	Status      string
+	Title       string
+	Description string
+	Date        string
+}
+
+type AllTasks struct {
+	TasksA []*Task
+}
+
+func GetAllTasks() *AllTasks {
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	datfile, err := io.ReadAll(file)
+	var aT AllTasks
+	json.Unmarshal(datfile, &aT.TasksA)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &aT
+}
