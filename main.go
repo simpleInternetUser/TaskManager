d9478df85d908c8492dc340a5d4c7faf3c7b0203
Taras <simple_internet_user@zohomail.com>
2022-08-01 22:11:08 +0300

    Реализованно отображение списка задач. Генерация id. Добавление новой таски

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/simpleInternetUser/TaskManager/tasks"
 )
@@ -16,7 +21,42 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, tasks.GetAllTasks())
 }
 
+func GetDate() (string, int) {
+	y, m, d := time.Now().Date()
+	str := fmt.Sprintf("%v %v %v", d, m, y)
+	t := time.Now()
+	id := t.Year() + t.Day() + int(t.Month()) + t.Hour() + t.Minute() + t.Second()
+	return str, id
+}
+
+func AddNewTask(w http.ResponseWriter, r *http.Request) {
+
+	newT := &tasks.Task{}
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("templates/addtask.html")
+		t.Execute(w, nil)
+	} else {
+
+		newT.Title = r.FormValue("title")
+		newT.Description = r.FormValue("description")
+		newT.Status = "new"
+		newT.Date, newT.Id = GetDate()
+
+		file, _ := os.OpenFile("tasks.json", os.O_RDWR, 0644)
+		defer file.Close()
+		var newUT tasks.AllTasks
+		bfile, _ := ioutil.ReadAll(file)
+		json.Unmarshal(bfile, &newUT.TasksA)
+		newUT.TasksA = append(newUT.TasksA, newT)
+		newData, _ := json.MarshalIndent(&newUT.TasksA, "", " ")
+		ioutil.WriteFile("tasks.json", newData, 0666)
+
+		http.Redirect(w, r, "/", 301)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", Index)
+	http.HandleFunc("/addtask/", AddNewTask)
 	http.ListenAndServe(":8080", nil)
 }
