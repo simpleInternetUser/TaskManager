817d6ee53d53613067d24111d78c0589a5b24928
Taras <simple_internet_user@zohomail.com>
2022-08-09 00:30:13 +0300

    Добавил пакет hendlers

---
+++ handlers/handlers.go
@@ -0,0 +1,102 @@
+package handlers
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	tasks "TaskManager/tasks"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, tasks.GetAllTasks())
+}
+
+func AddNewTask(w http.ResponseWriter, r *http.Request) {
+
+	newT := &tasks.Task{}
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("templates/addtask.html")
+		t.Execute(w, nil)
+	} else {
+
+		PageData(newT, r)
+
+		newUT := tasks.GetAllTasks()
+		newUT.TasksA = append(newUT.TasksA, newT)
+		newData, _ := json.MarshalIndent(&newUT.TasksA, "", " ")
+		ioutil.WriteFile("tasks.json", newData, 0666)
+
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+	}
+}
+
+func IndexJson(data *tasks.AllTasks, r *http.Request) int {
+	iddel, err := strconv.Atoi(r.FormValue("id"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	i := 0
+	for ; i < len(data.TasksA); i++ {
+		if data.TasksA[i].Id == iddel {
+			break
+		}
+	}
+	return i
+}
+
+func EditTask(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := template.ParseFiles("templates/edittask.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listT := tasks.GetAllTasks()
+	i := IndexJson(listT, r)
+	tmpl.ExecuteTemplate(w, "edit", listT.TasksA[i])
+	//PageData(listT.TasksA[i], r)
+
+	//http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+func SaveTask(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	http.Redirect(w, r, "/"+title, http.StatusFound)
+}
+func DelTask(w http.ResponseWriter, r *http.Request) {
+
+	listT := tasks.GetAllTasks()
+	i := IndexJson(listT, r)
+
+	listT.TasksA = append(listT.TasksA[:i], listT.TasksA[i+1:]...)
+	newData, _ := json.MarshalIndent(&listT.TasksA, "", " ")
+	ioutil.WriteFile("tasks.json", newData, 0666)
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+}
+
+func PageData(t *tasks.Task, r *http.Request) {
+	t.Title = r.FormValue("title")
+	t.Description = r.FormValue("description")
+	if t.Id == 0 {
+		t.Status = "new"
+		t.Date, t.Id = DateNowAndId()
+	} else {
+		t.Status = r.FormValue("status")
+	}
+}
+
+func DateNowAndId() (string, int) {
+	t := time.Now()
+	str := t.Format("01/02/2006 15:04:05")
+	id := t.Unix()
+	return str, int(id)
+}
--- main.go
+++ main.go
@@ -1,106 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"html/template"
-	"io/ioutil"
-	"log"
+	"hendlers/hendlers"
 	"net/http"
-	"strconv"
-	"time"
-
-	"github.com/simpleInternetUser/TaskManager/tasks"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl.Execute(w, tasks.GetAllTasks())
-}
-
-func AddNewTask(w http.ResponseWriter, r *http.Request) {
-
-	newT := &tasks.Task{}
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("templates/addtask.html")
-		t.Execute(w, nil)
-	} else {
-
-		PageData(newT, r)
-
-		newUT := tasks.GetAllTasks()
-		newUT.TasksA = append(newUT.TasksA, newT)
-		newData, _ := json.MarshalIndent(&newUT.TasksA, "", " ")
-		ioutil.WriteFile("tasks.json", newData, 0666)
-
-		http.Redirect(w, r, "/", http.StatusMovedPermanently)
-	}
-}
-
-func IndexJson(data *tasks.AllTasks, r *http.Request) int {
-	iddel, err := strconv.Atoi(r.FormValue("id"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	i := 0
-	for ; i < len(data.TasksA); i++ {
-		if data.TasksA[i].Id == iddel {
-			break
-		}
-	}
-	return i
-}
-
-func EditTask(w http.ResponseWriter, r *http.Request) {
-
-	tmpl, err := template.ParseFiles("templates/edittask.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	listT := tasks.GetAllTasks()
-	i := IndexJson(listT, r)
-	tmpl.ExecuteTemplate(w, "edit", listT.TasksA[i])
-	PageData(listT.TasksA[i], r)
-
-	http.Redirect(w, r, "/", http.StatusMovedPermanently)
-}
-
-func DelTask(w http.ResponseWriter, r *http.Request) {
-
-	listT := tasks.GetAllTasks()
-	i := IndexJson(listT, r)
-
-	listT.TasksA = append(listT.TasksA[:i], listT.TasksA[i+1:]...)
-	newData, _ := json.MarshalIndent(&listT.TasksA, "", " ")
-	ioutil.WriteFile("tasks.json", newData, 0666)
-
-	http.Redirect(w, r, "/", http.StatusMovedPermanently)
-}
-
-func PageData(t *tasks.Task, r *http.Request) {
-	t.Title = r.FormValue("title")
-	t.Description = r.FormValue("description")
-	if t.Id == 0 {
-		t.Status = "new"
-		t.Date, t.Id = DateNowAndId()
-	} else {
-		t.Status = r.FormValue("status")
-	}
-}
-
-func DateNowAndId() (string, int) {
-	t := time.Now()
-	str := t.Format("01/02/2006 15:04:05")
-	id := t.Unix()
-	return str, int(id)
-}
 func main() {
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/addtask/", AddNewTask)
-	http.HandleFunc("/edittask/", EditTask)
-	http.HandleFunc("/deltask/", DelTask)
+	http.HandleFunc("/", hendlers.Index)
+	http.HandleFunc("/addtask/", hendlers.AddNewTask)
+	http.HandleFunc("/edittask/", hendlers.EditTask)
+	http.HandleFunc("/savetask/", hendlers.SaveTask)
+	http.HandleFunc("/deltask/", hendlers.DelTask)
 	http.ListenAndServe(":8080", nil)
 }
